52786dba1d84ca338b989ae2e7c49117ac8043af
ayushpasi <pasiayush999@gmail.com>
2023-10-19 23:00:49 +0530

    Dynamic routes part 1

---
+++ Starting with Express Framework/7 Dynamic Routes Part 1/controllers/shop.js
@@ -0,0 +1,49 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  });
+};
+exports.getProduct=(req, res) => {
+  const prodId = req.params.productId;
+  // Do something with prodId, e.g., fetch data related to this product ID
+  Product.findById(prodId,product=>{
+   res.render("shop/product-detail",{product:product,pageTitle:product.title,path:'/products'})
+  });
+  console.log(prodId);
+}
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
